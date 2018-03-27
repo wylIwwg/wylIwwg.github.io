6d9063eeb495170f2807f7a4604f41db877b11eb
wylIwwg <wylaiaiai@qq.com>
2018-03-27 18:00:07 +0800

    aaa

--- Love_files/functions.js
+++ Love_files/functions.js
@@ -38,9 +38,12 @@ $(window).resize(function() {
 })(jQuery);
 
 function timeElapse(date){
-	var current = Date();
+	var current =new Date();
+	
+
 	var seconds = (Date.parse(current) - Date.parse(date)) / 1000;
 	var days = Math.floor(seconds / (3600 * 24));
+	
 	seconds = seconds % (3600 * 24);
 	var hours = Math.floor(seconds / 3600);
 	if (hours < 10) {
